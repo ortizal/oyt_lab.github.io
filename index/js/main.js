f137f0f8041a715ada7b5556f8839de2c2ab4d74
ortizal.github.io <alexis.ortiz81@outlook.com>
2020-08-29 16:01:21 -0500

    updated home page

---
+++ index/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+/*     $.ajax({
+        type:'POST',
+        url:'../php/clases.php'
+    })
+    .done(function(r){
+        $("#formulario").html(r);
+    })
+    .fail(function(){
+        alert("Error en la salida de las clases");
+    }); */
+
+
+})
+
+
+/*     $.ajax({
+        type:'POST',
+        url:'../php/subclases.php',
+        success:function(re){
+            $('#categoria').html(re);
+        }
+    }); */
